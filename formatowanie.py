64c287fcf9590b1e90f2b19aace181a0cd51a274
kornel <kppietrzyk@gmail.com>
2020-10-26 15:59:48 +0100

    wprowadzenie1

---
+++ formatowanie.py
@@ -0,0 +1,6 @@
+def zad3():
+    print('{:10}'.format('test'))
+    print('{:.5}'.format('oscyloskop'))
+    print('{:f}'.format(5181))
+    print('{:4d}'.format(210))
+    print('{:=+5d}'.format(23))
