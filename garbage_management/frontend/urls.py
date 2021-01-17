db0c7331154d941061f5e3840897215b9c56b686
Snnoky <robirobipl@gmail.com>
2021-01-18 00:34:20 +0100

    kps html

--- garbage_management/frontend/urls.py
+++ garbage_management/frontend/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('kierowca-smieciarki', login, name="login"),
     path('kierownik-glowny', login, name="login"),
     path('kierownik-przewozu-smieci', index_kps, name="index kps"),
+    path('kierownik-przewozu-smieci/uzytkownicy', users_kps, name="users kps"),
     path('kierownik-wysypiska', login, name="login"),
     path('ksiegowosc', login, name="login"),
     path('pracownicy-przewozacy-smieci', login, name="login"),
