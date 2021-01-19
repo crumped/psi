2500dd53d3755ea199f39cfcfaae1e6998582e48
kornel <kppietrzyk@gmail.com>
2021-01-20 00:59:26 +0100

    add placeholders to forms

--- garbage_management/frontend/forms.py
+++ garbage_management/frontend/forms.py
@@ -2,15 +2,22 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    login = forms.CharField(label='Nazwa użytkownika', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    login = forms.CharField(label='Nazwa użytkownika', max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Nazwa użytkownika'}))
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'placeholder': 'Hasło'}))
 
 
 class AddUserForm(forms.Form):
-    username = forms.CharField(label='Nazwa użytkownika', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    firstname = forms.CharField(label="Imię", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    lastname = forms.CharField(label="Nazwisko",max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    adress = forms.CharField(label="Adres",max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Nazwa użytkownika', max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Nazwa użytkownika'}))
+    password = forms.CharField(label="Hasło", widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                'placeholder': 'Hasło'}))
+    firstname = forms.CharField(label="Imię", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                                            'placeholder': 'Imię'}))
+    lastname = forms.CharField(label="Nazwisko", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                                               'placeholder': 'Nazwisko'}))
+    adress = forms.CharField(label="Adres", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                                          'placeholder': 'Adres'}))
     CHOICES = (('kierowca-smieciarki', 'Kierowca Śmieciarki'), ('pracownik-przewazacy-smieci', 'Pracownik Śmieciarki'),)
-    group = forms.ChoiceField(label="Stanowisko",choices=CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
+    group = forms.ChoiceField(label="Stanowisko", choices=CHOICES, widget=forms.Select(attrs={'class': 'form-select'}))
